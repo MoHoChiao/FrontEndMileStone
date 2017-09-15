535786a5b205b3303f9ee5476b6394ed2cd29be4
hungw <hungweiliu543@gmail.com>
2017-09-15 11:29:34 +0800

    update

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-    baseURL: `http://127.0.0.1:8001/`,
+    baseURL: `http://192.168.7.63:8001/`,
     timeout: 5000,
     withCredentials: true,
     headers: {
