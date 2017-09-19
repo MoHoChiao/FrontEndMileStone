c25ec810f909b6a7af23fe34c51c1773df9f47c2
hungw <hungweiliu543@gmail.com>
2017-09-19 10:51:11 +0800

    update

--- src/axios/http-common.js
+++ src/axios/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-    baseURL: `http://192.168.7.63:8001/`,
+    baseURL: `http://192.168.7.63:8080/frontend-trinity-service/`,
     timeout: 5000,
     withCredentials: true,
     headers: {
