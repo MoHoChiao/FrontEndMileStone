d855fcdbd2d66c607287c884674834e6288d434e
User <User@DESKTOP-15MK05L.mshome.net>
2018-09-28 11:16:13 +0800

    如果是在本地eclipse跑, 則application name改填入為空白, 例如`http://` + '127.0.0.1:8080' + ``, 如果是build出來在任何container(tomcat, jetty)的話, 此空白填入war檔名稱, 這裡我們build出來是叫ResourceAdminService.war, build之前記得改成`http://` + '127.0.0.1:8080' + `/ResourceAdminService/`

--- src/util_js/axios_util.js
+++ src/util_js/axios_util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP_AUTH = axios.create({
-    baseURL: `http://` + '127.0.0.1:8080' + `/ResourceAdminService/`,
+    baseURL: `http://` + '127.0.0.1:8080' + ``,
     timeout: 10000,
     withCredentials: true,
     headers: {
@@ -14,7 +14,7 @@ export const HTTP_AUTH = axios.create({
     },
 })
 export const HTTP_TRINITY = axios.create({
-    baseURL: `http://` + '127.0.0.1:8080' + `/ResourceAdminService/`,
+    baseURL: `http://` + '127.0.0.1:8080' + ``,
     timeout: 10000,
     withCredentials: true,
     headers: {
@@ -27,7 +27,7 @@ export const HTTP_TRINITY = axios.create({
     },
 })
 export const HTTP_TRINITY_Upload = axios.create({
-    baseURL: `http://` + '127.0.0.1:8080' + `/ResourceAdminService/`,
+    baseURL: `http://` + '127.0.0.1:8080' + ``,
     timeout: 100000,
     withCredentials: true,
     headers: {
@@ -40,7 +40,7 @@ export const HTTP_TRINITY_Upload = axios.create({
     },
 })
 export const HTTP_TRINITY_Download = axios.create({
-    baseURL: `http://` + '127.0.0.1:8080' + `/ResourceAdminService/`,
+    baseURL: `http://` + '127.0.0.1:8080' + ``,
     timeout: 60000,
     withCredentials: true,
     responseType: 'blob',
